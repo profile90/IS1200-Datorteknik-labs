ab79bb067a65db910235c3071537c619fa842249
profile90 <15sagr@stockholmscience.se>
2019-01-22 13:28:57 +0100

    Fixed masking error, that would turn the output weird.

--- lab1/assignment2.asm
+++ lab1/assignment2.asm
@@ -22,7 +22,8 @@ stop:	j	stop		# stop after one run
 
 
 hexasc:
-	addi $t0, $a0, 0x30 	# Add (int)'0' so that the input matches the ascii-table for '0'
+	andi $t0, $a0, 0xf	# Mask off all bits past 0xf, so there is some period to the input (17 becomes 2), etc
+	addi $t0, $t0, 0x30 	# Add (int)'0' so that the input matches the ascii-table for '0'
 	blt  $a0, 10, decimal	# Check if less than 10, thus still decimal
 	addi $t0, $t0, 0x27	# Add ((int)'a' - 10) so that the input matches the ascii-table for 'a' (48 + (49 - 10) = 48 + 39 = 97)
 decimal:
